169fedb5a4897a3069ff8fc957db76d64f17c225
Lidiane Pereira <lidiane.pereira@dtidigital.com.br>
2021-04-26 17:30:58 -0300

    novas alteraçoes

--- lib/home/widgets/quizCard/quiz_card_widget.dart
+++ lib/home/widgets/quizCard/quiz_card_widget.dart
@@ -1,5 +1,4 @@
 import 'package:devQuiz/core/app_colors.dart';
-import 'package:devQuiz/core/app_images.dart';
 import 'package:devQuiz/core/app_text_styles.dart';
 import 'package:devQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
